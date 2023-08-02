0f6287bd0595691c12793625e46e20b08d6beb65
sandhya454 <tsandhya2014@gmail.com>
2023-08-02 13:25:45 +0530

    complt search funt to subcategeory

--- src/Navigation_page/Pages/Catergory_Setup/sub_categories.jsx
+++ src/Navigation_page/Pages/Catergory_Setup/sub_categories.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import {  MenuApp, Pencil, Search, Trash } from 'react-bootstrap-icons';
 import '../../CSS/SubCategories.css'
 export default function SubCategories() {
+
+    const tableData=[
+        {id:1, img:"Meat and Fish", name:"Tofu and meat alternatives",status:"", icon1:<Pencil/>,icon2:<Trash/>},
+        {id:2, img:"Meat and Fish", name:"Dried Fish",status:"", icon1:<Pencil/>,icon2:<Trash/>},
+        {id:3, img:"Meat and Fish", name:"Fresh Fish",status:"", icon1:<Pencil/>,icon2:<Trash/>},
+        {id:4, img:"Fruits and Vegetables", name:"Meat",status:"", icon1:<Pencil/>,icon2:<Trash/>},
+        {id:5, img:"Meat and Fish", name:"Fresh Vegetables",status:"", icon1:<Pencil/>,icon2:<Trash/>},
+        {id:6, img:"Fruits and Vegetables", name:"Fresh Fruits",status:"", icon1:<Pencil/>,icon2:<Trash/>},
+        {id:7, img:"Fruits and Vegetables", name:"Tofu and meat alternatives",status:"", icon1:<Pencil/>,icon2:<Trash/>},
+    ]
+
+    const [value,setValue]=useState('')
+
+    const [dataSource,setDataSource]=useState(tableData);
+    const [tableFilter,setTableFilter]=useState([])
+
+
+    const filterData=(e)=>{
+        if(e.target.value!=''){
+            setValue(e.target.value)
+            const filterTable= dataSource.filter(o=>Object.keys(o).some(k=>
+                String(o[k]).toLowerCase().includes(e.target.value.toLowerCase())
+            ))
+            setTableFilter([...filterTable])
+        }else{
+            setValue(e.target.value)
+            setDataSource([...dataSource])
+        }
+    }
   return ( 
     
     <>
@@ -46,7 +75,7 @@ export default function SubCategories() {
                             <div className='card--header'>
                                 <h5 className='card-title'> Category details</h5>
                            <form>
-                            <div className='input-group'> <input type="search" className='form-control' aria-label='Search' placeholder='search by name'/>
+                            <div className='input-group'> <input type="search" className='form-control' aria-label='Search' placeholder='search by name' value={value} onChange={filterData}/>
                             <span className='tio-input-search '><Search/></span>
                             <div class="input-group-append">
                                 <button type="submit" class="input-group-text" fdprocessedid="maarpr"> Search</button>
@@ -65,65 +94,34 @@ export default function SubCategories() {
                                         <th>Status</th>
                                         <th class="text-center">Action</th>
                                         </tr>
+
+                                       <tbody>
+                                       {value.length>0 ? tableFilter.map((data)=>{
+                                        return(
+                                            <tr>
+                                        <td>{data.id}</td>
+                                        <td >{data.img}</td>
+                                        <td>{data.name}</td>
+                                        <td>{data.status}</td>
+                                        <td><button className='icon1'>{data.icon1}</button><button className='icon2'>{data.icon2}</button></td>
+                                    </tr>
+                                        )
+                                    }):
+                                    dataSource.map((data)=>{
+                                        return(
+                                            <tr>
+                                        <td>{data.id}</td>
+                                        <td >{data.img}</td>
+                                        <td>{data.name}</td>
+                                        <td>{data.status}</td>
+                                        <td><button className='icon1'>{data.icon1}</button><button className='icon2'>{data.icon2}</button></td>
+                                    </tr>
+                                        )
+                                    })
+                                    }
                                         
+                                        </tbody> 
                             
-                                <tr>
-                                    <td>1</td>
-                                    <td>Meat and fish</td>
-                                    <td>Tofu & Meat Alternatives</td>
-                                    <td className='text-center'><div class="form-check form-switch"><input class="form-check-input" type="checkbox" id="flexSwitchCheckChecked" checked/></div></td>
-                                    <td >
-                                    <button className='icon1'><Pencil/></button> <button className='icon2'><Trash/></button>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td>2</td>
-                                    <td>Meat and fish</td>
-                                    <td> Dried Fish</td>
-                                    <td className='text-center'><div class="form-check form-switch"><input class="form-check-input" type="checkbox" id="flexSwitchCheckChecked" checked/></div></td>
-                                    <td >
-                                    <button className='icon1'><Pencil/></button> <button className='icon2'><Trash/></button>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td>3</td>
-                                    <td>Meat and fish</td>
-                                    <td> Fresh Fish</td>
-                                    <td className='text-center'><div class="form-check form-switch"><input class="form-check-input" type="checkbox" id="flexSwitchCheckChecked" checked/></div></td>
-                                    <td >
-                                    <button className='icon1'><Pencil/></button> <button className='icon2'><Trash/></button>
-                                    </td>
-                                </tr>
-
-                                <tr>
-                                    <td>4</td>
-                                    <td>Meat and fish</td>
-                                    <td> Meat</td>
-                                    <td className='text-center'><div class="form-check form-switch"><input class="form-check-input" type="checkbox" id="flexSwitchCheckChecked" checked/></div></td>
-                                    <td >
-                                    <button className='icon1'><Pencil/></button> <button className='icon2'><Trash/></button>
-                                    </td>
-                                </tr>
-
-                                <tr>
-                                    <td>5</td>
-                                    <td>Fruits and Vegetables</td>
-                                    <td> Fresh Vegetables</td>
-                                    <td className='text-center'><div class="form-check form-switch"><input class="form-check-input" type="checkbox" id="flexSwitchCheckChecked" checked/></div></td>
-                                    <td >
-                                    <button className='icon1'><Pencil/></button> <button className='icon2'><Trash/></button>
-                                    </td>
-                                </tr>
-
-                                <tr>
-                                    <td>6</td>
-                                    <td>Fruits and Vegetables</td>
-                                    <td> Fresh Fruits</td>
-                                    <td className='text-center'><div class="form-check form-switch"><input class="form-check-input" type="checkbox" id="flexSwitchCheckChecked" checked/></div></td>
-                                    <td >
-                                    <button className='icon1'><Pencil/></button> <button className='icon2'><Trash/></button>
-                                    </td>
-                                </tr>
                             
                             </table>
                         </div>
